2ceb35d8982fe205dddc6ef8e2554db25775af65
SumukhBhende <sumukhsbhende@gmail.com>
2025-06-29 18:12:04 +0530

    feat: integrate react-router-dom for routing in the application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './globals.css';
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="flex h-screen">
+        <Routes>
+            {/* Public Routes */}
+            <Route path="/sign-in" element ={<SigninForm/>}/>
+            {/*Private Routes*/}
+            <Route index element={<Home />} />
+        </Routes>
+    </main>
   )
 };
 
